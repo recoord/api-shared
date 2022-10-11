17d1d57923d5aeedbb7648162f26654264e621c3
Martin Treacy-Schwartz <mts@veo.co>
2022-10-11 13:56:20 +0200

    moved job create request model into shared

---
+++ models/api/JobCreateRequestV1.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using Swashbuckle.AspNetCore.Annotations;
+
+[SwaggerSchema(Description = "Job create request")]
+public record JobCreateRequestV1
+{
+    [Required]
+    [SwaggerSchemaExample("argo")]
+    [MinLength(1, ErrorMessage = "The JobSystemKind must be {1} characters long.")]
+    public string JobSystemKind { get; init; } = "";
+
+    [Required]
+    [SwaggerSchemaExample("/workflows/processing-argo/submit")]
+    public string JobSystemCreateUrlPath { get; init; } = "";
+
+    [SwaggerSchemaExample("/workflows/processing-argo/{0}/stop")]
+    public string JobSystemAbortUrlPath { get; init; } = "";
+
+    [Range(0.1, 60 * 24, ErrorMessage = "Job timeout must be between {1} and {2}.")]
+    public double JobTimeoutMinutes { get; init; } = 60 * 24;
+
+    public SystemAuthV1? SystemAuth { get; init; }
+
+    public JobSystemArgsV1? JobSystemArgs { get; init; }
+
+    public TagsV1? Tags { get; init; }
+
+    public NotificationV1? Notifications { get; init; }
+
+    public SynchronizedV1? Synchronized { get; init; }
+}
