09a27e89e048cd0e5abc66346bf2561da93a91e0
Martin Treacy-Schwartz <mts@veo.co>
2022-10-11 13:45:27 +0200

    added job create response model

---
+++ models/api/JobCreateResponseV1.cs
@@ -0,0 +1,8 @@
+using Swashbuckle.AspNetCore.Annotations;
+
+[SwaggerSchema("Output of the create job request")]
+public record JobCreateResponseV1
+{
+    [SwaggerSchema(Description = "Info on the created job", Nullable = true)]
+    public JobV1? Job { get; init; }
+}
