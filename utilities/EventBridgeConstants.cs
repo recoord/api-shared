e6a8455e482a5188e986b3400bce3f4c4c9d2a36
Martin Treacy-Schwartz <mts@veo.co>
2022-10-11 09:19:55 +0200

    added event bridge constants

---
+++ utilities/EventBridgeConstants.cs
@@ -0,0 +1,9 @@
+public static class EventBridgeConstants
+{
+    #region Detail types
+    public static readonly string DetailTypeRecordingUploaded = "recording-uploaded";
+    #endregion // Detail types
+
+    #region Event source
+    public static readonly string SourceRecordings = "recordings-api";
+}
