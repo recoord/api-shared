150504fba8f5ebd39cf372a36fa24d753548827a
Martin Treacy-Schwartz <mts@veo.co>
2022-10-11 14:05:49 +0200

    moved models into shared

---
+++ models/api/argo/ArgoWorkflowArgsV1.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+using Swashbuckle.AspNetCore.Annotations;
+
+[ExcludeFromCodeCoverage]
+[SwaggerSchema(Description = "The argo workflow arguments")]
+public class ArgoWorkflowArgsV1
+{
+    [SwaggerSchemaExample("processing-argo")]
+    [JsonPropertyName("namespace")]
+    public string? Namespace { get; set; }
+
+    [SwaggerSchemaExample("WorkflowTemplate")]
+    public string? resourceKind { get; set; }
+
+    [SwaggerSchemaExample("stacked-rendering")]
+    public string? resourceName { get; set; }
+
+    public SubmitOptions? submitOptions { get; set; }
+}
---
+++ models/api/argo/SubmitOptions.cs
@@ -0,0 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
+using Swashbuckle.AspNetCore.Annotations;
+
+[ExcludeFromCodeCoverage]
+public class SubmitOptions
+{
+    [SwaggerSchema(Description = "Parameters needed for the workflow. Jobs API will append job id (with 'job-api-id' key) for Argo to use to send back job state updates.", Nullable = true)]
+    [SwaggerSchemaExample("tsvideo-s3-link=https://c.veocdn.com/e9e1fb11-98c2-4dd8-ba16-4cdcfc5b283a/panorama/video.ts")]
+    public IList<string>? parameters { get; set; }
+}
