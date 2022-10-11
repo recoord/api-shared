50cc1d5cac8a6ba53cc080b00ff9ae71b053c860
Martin Treacy-Schwartz <mts@veo.co>
2022-10-11 09:29:52 +0200

    added job state changed detail type

--- utilities/EventBridgeConstants.cs
+++ utilities/EventBridgeConstants.cs
@@ -2,8 +2,11 @@ public static class EventBridgeConstants
 {
     #region Detail types
     public static readonly string DetailTypeRecordingUploaded = "recording-uploaded";
+    public static readonly string DetailTypeJobStateChanged = "job-state-changed";
     #endregion // Detail types
 
     #region Event source
     public static readonly string SourceRecordings = "recordings-api";
+    public static readonly string SourceJobs = "jobs-api";
+    #endregion // Event source
 }
