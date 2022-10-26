4ecac8c97c618e67f1ab5352704666a320208c45
Martin Treacy-Schwartz <mts@veo.co>
2022-10-26 15:41:44 +0200

    added job state notification model

---
+++ models/api/JobStateNotificationV1.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using Swashbuckle.AspNetCore.Annotations;
+
+public record JobStateNotificationV1
+{
+    [SwaggerSchema(Description = "Job id")]
+    [SwaggerSchemaExample("5ddf4520-2472-4666-973e-9a691850152a")]
+    [Required]
+    [NotEmptyGuid]
+    public Guid JobId { get; init; }
+
+    [Required]
+    [ValidEnum(AllowZeroValues = false)]
+    public JobStateV1 JobState { get; init; }
+
+    [SwaggerSchema(Description = "Timestamp in UTC format where job state changed happened in jobs service")]
+    [SwaggerSchemaExample("2022-08-10T14:14:41Z")]
+    [Required]
+    public DateTime StateChangedAt { get; init; }
+}
