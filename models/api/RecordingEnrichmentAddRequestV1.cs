4380cd81300a103171c8b424187395b941325407
Martin Treacy-Schwartz <mts@veo.co>
2022-10-27 08:51:57 +0200

    moved enrichment add request to shared

---
+++ models/api/RecordingEnrichmentAddRequestV1.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using Swashbuckle.AspNetCore.Annotations;
+
+[SwaggerSchema(Description = "Recording enrichment add request")]
+public record RecordingEnrichmentAddRequestV1
+{
+    [SwaggerSchema(Description = "Recording id")]
+    [SwaggerSchemaExample("5ddf4520-2472-4666-973e-9a691850152a")]
+    [Required]
+    [NotEmptyGuid]
+    public Guid RecordingId { get; init; }
+
+    [Required]
+    [MinLength(1, ErrorMessage = "The enrichment name field must not be empty")]
+    public string EnrichmentName { get; init; } = "";
+
+    [Required]
+    [MinLength(1, ErrorMessage = "Url for recording enrichment")]
+    public string Url { get; init; } = "";
+}
