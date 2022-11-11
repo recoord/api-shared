adf5bf7d752af2bd3be71b3c79d1cec31f519570
Martin Treacy-Schwartz <mts@veo.co>
2022-11-12 00:54:49 +0100

    added s3 eventbridge values

--- utilities/EventBridgeConstants.cs
+++ utilities/EventBridgeConstants.cs
@@ -3,10 +3,12 @@ public static class EventBridgeConstants
     #region Detail types
     public static readonly string DetailTypeRecordingUploaded = "recording-uploaded";
     public static readonly string DetailTypeJobStateChanged = "job-state-changed";
+    public static readonly string DetailTypeS3ObjectCreated = "Object Created";
     #endregion // Detail types
 
     #region Event source
     public static readonly string SourceRecordings = "recordings-api";
     public static readonly string SourceJobs = "jobs-api";
+    public static readonly string SourceAwsS3 = "aws.s3";
     #endregion // Event source
 }
