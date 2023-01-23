55d39275f8b36f0a231d44b260862f95cd84eb64
Martin Treacy-Schwartz <mts@veo.co>
2023-01-23 13:02:20 +0100

    updated recording model

--- models/api/RecordingV1.cs
+++ models/api/RecordingV1.cs
@@ -7,9 +7,19 @@ public record RecordingV1
     [SwaggerSchemaExample("5ddf4520-2472-4666-973e-9a691850152a")]
     public Guid RecordingId { get; init; }
 
-    [SwaggerSchema(Description = "Url for video.ts file")]
+    [SwaggerSchema(Description = "S3 uri for video.ts file")]
+    [SwaggerSchemaExample("s3://veo-stag-content/7848738c-2e16-4220-b0d8-43257f8dea38/video.ts")]
+    public string? VideoTsS3Uri { get; init; }
+
+    [SwaggerSchema(Description = "Object url for video.ts file")]
+    [SwaggerSchemaExample("https://veo-stag-content.s3.eu-west-1.amazonaws.com/7848738c-2e16-4220-b0d8-43257f8dea38/video.ts")]
     public string? VideoTsUrl { get; init; }
 
-    [SwaggerSchema(Description = "Url for upload.veo file")]
+    [SwaggerSchema(Description = "S3 uri for upload.veo file")]
+    [SwaggerSchemaExample("s3://veo-stag-content/7848738c-2e16-4220-b0d8-43257f8dea38/upload.veo")]
+    public string? UploadVeoS3Uri { get; init; }
+
+    [SwaggerSchema(Description = "Object url for upload.veo file")]
+    [SwaggerSchemaExample("s3://veo-stag-content/7848738c-2e16-4220-b0d8-43257f8dea38/upload.veo")]
     public string? UploadVeoUrl { get; init; }
 }
