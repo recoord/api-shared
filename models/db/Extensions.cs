aa49bcc05b656bc1f5d7de425effb8573ec33418
Martin Treacy-Schwartz <mts@veo.co>
2023-01-24 11:05:57 +0100

    added paged results model

---
+++ models/db/Extensions.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+
+public static class DbExtensions
+{
+    public static async Task<PagedResult<T>> GetPagedAsync<T>(this IQueryable<T> query, int page, int pageSize) where T : class
+    {
+        var result = new PagedResult<T>();
+        result.CurrentPage = page;
+        result.PageSize = pageSize;
+        result.RowCount = await query.CountAsync();
+
+        var pageCount = (double)result.RowCount / pageSize;
+        result.PageCount = (int)Math.Ceiling(pageCount);
+
+        var skip = (page - 1) * pageSize;
+        result.Results = await query.Skip(skip).Take(pageSize).ToListAsync();
+
+        return result;
+    }
+
+    public static PagedResult<T> GetPaged<T>(this IQueryable<T> query, int page, int pageSize) where T : class
+    {
+        var result = new PagedResult<T>();
+        result.CurrentPage = page;
+        result.PageSize = pageSize;
+        result.RowCount = query.Count();
+
+        var pageCount = (double)result.RowCount / pageSize;
+        result.PageCount = (int)Math.Ceiling(pageCount);
+
+        var skip = (page - 1) * pageSize;
+        result.Results = query.Skip(skip).Take(pageSize).ToList();
+
+        return result;
+    }
+}
