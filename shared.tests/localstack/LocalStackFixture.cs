34b207929383ef2a0a329d5abb56a8f583cca7a6
Martin Treacy-Schwartz <mts@veo.co>
2023-04-13 13:45:21 +0200

    split up into normal project and tests project

---
+++ shared.tests/localstack/LocalStackFixture.cs
@@ -0,0 +1,31 @@
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using Xunit;
+
+public class LocalStackFixture : IAsyncLifetime
+{
+    private readonly TestcontainersContainer _localStackContainer;
+
+    public LocalStackFixture()
+    {
+        var localStackBuilder = new TestcontainersBuilder<TestcontainersContainer>()
+            .WithImage("localstack/localstack")
+            .WithCleanUp(true)
+            .WithEnvironment("DEFAULT_REGION", "eu-west-1")
+            .WithEnvironment("SERVICES", "sqs,s3,events")
+            .WithEnvironment("DOCKER_HOST", "unix:///var/run/docker.sock")
+            .WithEnvironment("DEBUG", "1")
+            .WithPortBinding(4567, 4566);
+
+        _localStackContainer = localStackBuilder.Build();
+    }
+    public async Task InitializeAsync()
+    {
+        await _localStackContainer.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _localStackContainer.StopAsync();
+    }
+}
