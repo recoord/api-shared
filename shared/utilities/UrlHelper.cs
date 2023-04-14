99e3203e45dcc9d013bba073099d9ef65f212208
Martin Treacy-Schwartz <mts@veo.co>
2023-04-14 11:13:06 +0200

    added url helper

---
+++ shared/utilities/UrlHelper.cs
@@ -0,0 +1,22 @@
+public static class UrlHelper
+{
+    public static string? GenerateS3Uri(string? bucket, string? key)
+    {
+        if (bucket == null || key == null)
+        {
+            return null;
+        }
+
+        return $"s3://{bucket}/{key}";
+    }
+
+    public static string? GenerateUrl(string? bucket, string? key)
+    {
+        if (bucket == null || key == null)
+        {
+            return null;
+        }
+
+        return $"https://{bucket}.s3.amazonaws.com/{key}";
+    }
+}
