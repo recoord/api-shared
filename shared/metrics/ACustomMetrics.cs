be62ea40a6bf64ec4cc1a5ad14c0cf79dd3b5d68
Martin Treacy-Schwartz <mts@veo.co>
2023-04-19 11:07:47 +0200

    added base custom metrics class

---
+++ shared/metrics/ACustomMetrics.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.Configuration;
+
+public abstract class ACustomMetric
+{
+    private readonly string _serviceName;
+
+    public ACustomMetric(IConfiguration config)
+    {
+        _serviceName = config["Application:Name"] ?? "service-name-not-present";
+    }
+
+    protected string GenerateServiceName(string name)
+    {
+        return $"{_serviceName}_{name}";
+    }
+}
