56328b30747a95ad056b54bc99a3c09c421ed5a9
Martin Treacy-Schwartz <mts@veo.co>
2023-04-24 16:46:34 +0200

    disabled exemplars in prometheus

--- shared/metrics/MetricsDependecyInjection.cs
+++ shared/metrics/MetricsDependecyInjection.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using Prometheus;
 
 public static class MetricsDependecyInjection
 {
@@ -6,6 +7,7 @@ public static class MetricsDependecyInjection
     {
         if (startHostedService == false) { return; }
 
+        Metrics.DefaultFactory.ExemplarBehavior = ExemplarBehavior.NoExemplars();
         services.AddHostedService<MetricHostedService>();
     }
 }
