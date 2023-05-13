d8f1b0b4ade577a04943e3633439b19f2ed1a820
Martin Treacy-Schwartz <mts@veo.co>
2023-05-13 09:36:04 +0200

    added no use in prod validation attribute

---
+++ shared/validation/NoUseInProdAttribute.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+[AttributeUsage(
+    AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
+    AllowMultiple = false)]
+public class NoUseInProdAttribute : ValidationAttribute
+{
+    public const string DefaultErrorMessage = "The {0} field must not be used in production";
+    public NoUseInProdAttribute() : base(DefaultErrorMessage) { }
+
+    public override bool IsValid(object? value)
+    {
+        //NotEmpty doesn't necessarily mean required
+        if (value is null)
+        {
+            return true;
+        }
+
+        return !EnvironmentHelper.IsProdEnvironment();
+    }
+}
