42a9e127d90aec65074e02133653c17c0c9e1bdb
Martin Treacy-Schwartz <mts@veo.co>
2023-05-25 17:14:05 +0200

    added extension to http status code class

--- shared/utilities/extensions/HttpStatusCodeExtensions.cs
+++ shared/utilities/extensions/HttpStatusCodeExtensions.cs
@@ -7,4 +7,15 @@ public static class HttpStatusCodeExtensions
         var asInt = (int)statusCode;
         return asInt >= 200 && asInt <= 299;
     }
+
+    public static bool ShouldRetry(this HttpStatusCode statusCode)
+    {
+        var asInt = (int)statusCode;
+        return asInt == 408 ||
+            asInt == 425 ||
+            asInt == 429 ||
+            asInt == 502 ||
+            asInt == 503 ||
+            asInt == 504;
+    }
 }
