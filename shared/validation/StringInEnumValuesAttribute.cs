64662816a50c0b85e2d9bba0eb241e6bd40c787b
Martin Treacy-Schwartz <mts@veo.co>
2023-08-31 09:02:01 +0200

    added string in enum values attribute

---
+++ shared/validation/StringInEnumValuesAttribute.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+
+[AttributeUsage(
+    AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
+    AllowMultiple = false)]
+public class StringInEnumValuesAttribute : ValidationAttribute
+{
+    public Type Type { get; set; }
+
+    public StringInEnumValuesAttribute(Type type)
+    {
+        Type = type;
+    }
+
+    public override string FormatErrorMessage(string name)
+    {
+        var enumValuesString = String.Join("|", (new List<string>(Enum.GetNames(Type))).ConvertAll(s => s.ToLowerInvariant()));
+        return name + " must be one of the following values " + enumValuesString;
+    }
+
+    public override bool IsValid(object? value)
+    {
+        if (value is null)
+        {
+            return false;
+        }
+
+        switch (value)
+        {
+            case string str:
+                return Enum.TryParse(Type!, str, true, out object? result);
+            default:
+                return false;
+        }
+    }
+}
